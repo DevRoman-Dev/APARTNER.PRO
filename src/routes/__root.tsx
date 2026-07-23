8f18fd98a7ad7a6c03fc0a4bc6620d4a30d0a375
Roman Matviy <roman@matviy.pp.ua>
2026-07-23 21:10:50 +0300

    perf: preload all font weights to eliminate remaining network dependency chains

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -89,7 +89,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "preload", as: "font", type: "font/woff2", href: "/fonts/inter/inter-latin-400-normal.woff2", crossOrigin: "anonymous" },
+      { rel: "preload", as: "font", type: "font/woff2", href: "/fonts/inter/inter-latin-500-normal.woff2", crossOrigin: "anonymous" },
+      { rel: "preload", as: "font", type: "font/woff2", href: "/fonts/inter/inter-latin-700-normal.woff2", crossOrigin: "anonymous" },
       { rel: "preload", as: "font", type: "font/woff2", href: "/fonts/inter/inter-latin-800-normal.woff2", crossOrigin: "anonymous" },
+      { rel: "preload", as: "font", type: "font/woff2", href: "/fonts/jetbrains-mono/jetbrains-mono-latin-400-normal.woff2", crossOrigin: "anonymous" },
+      { rel: "preload", as: "font", type: "font/woff2", href: "/fonts/jetbrains-mono/jetbrains-mono-latin-500-normal.woff2", crossOrigin: "anonymous" },
       ...(import.meta.env.DEV ? [{ rel: "stylesheet", href: appCssUrl }] : []),
     ],
   }),
